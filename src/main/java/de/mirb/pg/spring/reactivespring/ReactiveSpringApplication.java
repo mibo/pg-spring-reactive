73ca56527b146bb8d5f60adc502c8520870e4cc1
mibo <mibo@mirb.de>
2017-05-15 21:19:45 +0200

    Initial project setup

---
+++ src/main/java/de/mirb/pg/spring/reactivespring/ReactiveSpringApplication.java
@@ -0,0 +1,12 @@
+package de.mirb.pg.spring.reactivespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringApplication.class, args);
+	}
+}

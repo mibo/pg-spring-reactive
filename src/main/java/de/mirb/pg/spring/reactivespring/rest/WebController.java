09938bee8937605161e0d8b92c87c93ef33b35c8
mibo <mibo@mirb.de>
2017-05-16 22:31:33 +0200

    Added infinite stream

--- src/main/java/de/mirb/pg/spring/reactivespring/rest/WebController.java
+++ src/main/java/de/mirb/pg/spring/reactivespring/rest/WebController.java
@@ -1,13 +1,18 @@
 package de.mirb.pg.spring.reactivespring.rest;
 
 import de.mirb.pg.spring.reactivespring.domain.Event;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
+import java.time.Duration;
+import java.util.Date;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 @RestController
@@ -20,9 +25,20 @@ public class WebController {
 
   @GetMapping(path = "/events")
   public Flux<Event> readEvents(@RequestHeader(value = "amount", defaultValue = "10", required = false) int amount) {
-    Flux<Event> response = Flux.fromStream(eventStream(amount));
+//    if(amount <= 0) {
+//      return Flux.fromStream(eventStreamInfinite(10));
+//    }
+    return Flux.fromStream(eventStream(amount));
+  }
 
-    return response;
+  @GetMapping(path = "/infinite", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public Flux<Event> infinite() {
+    Stream<Event> evStream = Stream.generate(() ->
+        new Event(UUID.randomUUID().toString().substring(24), new Date()));
+    Flux<Event> eventFlux = Flux.fromStream(evStream);
+    Flux<Long> interval = Flux.interval(Duration.ofSeconds(1));
+
+    return Flux.zip(eventFlux, interval).map(Tuple2::getT1);
   }
 
   @GetMapping(path = "/events/{id}")
@@ -31,6 +47,20 @@ public class WebController {
 
     return response;
   }
+
+//  private Stream<Event> eventStreamInfinite(int amount) {
+//    return Stream.generate(() -> new Event("Name.." + nextId(amount)));
+//  }
+//
+//  private int counter = -1;
+//  private int nextId(int amount) {
+//    if(counter < 0) {
+//      counter = amount;
+//    } else if(counter == 0) {
+//      return 0;
+//    }
+//    return counter--;
+//  }
 
   private Stream<Event> eventStream(int amount) {
     Event[] events = new Event[amount];
